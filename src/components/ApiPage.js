c7c27854af3a80f72c8132d910b45a369eb7f814
Alex Fricker <africker@myriad.com>
2022-02-07 13:20:46 -0500

    api path updates - needs to be env config

--- src/components/ApiPage.js
+++ src/components/ApiPage.js
@@ -3,12 +3,13 @@ import Spinner from "./Spinner";
 
 class Api extends React.Component {
   componentDidMount() {
-    window.location.href = "http://localhost/";
+    window.location.href = "https://ui-dev-mde.apps.tst.openshift-stg-ext.aws.myriad.com/";
   }
 
-  render() {
-    return <Spinner message="Redirecting.." />;
-  }
+   render() {
+  return <Spinner message="Redirecting.." />;
+   }
+  
 }
 
 export default Api;
