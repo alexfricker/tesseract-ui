dd6bc7ecc4fc18b85ab08808ecb9b6e194739f8c
Alex Fricker <africker@myriad.com>
2022-02-07 12:35:39 -0500

    typo in auth header template

--- src/api/tesseract.js
+++ src/api/tesseract.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: "https://ui-dev-mde.apps.tst.openshift-stg-ext.aws.myriad.com/",
-    headers: {Authorization: localStorage.getItem('jwtToken')},
+    headers: {Authorization: "Bearer " + localStorage.getItem('jwtToken')},
 });
